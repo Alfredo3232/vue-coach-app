b73f9b0bdf50f6a660e2129d47fe79648d83fbf1
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-25 16:33:00 -0600

    add ui components to other components

--- src/components/coaches/CoachItem.vue
+++ src/components/coaches/CoachItem.vue
@@ -6,8 +6,8 @@
             <span v-for="area in areas" :key="area">{{ area }}</span>
         </div>
         <div class="actions">
-            <router-link :to="coachContactLink">Contact</router-link>
-            <router-link :to="coachDetailsLink">View Details</router-link>
+            <base-button mode="outline" link="" :to="coachContactLink">Contact</base-button>
+            <base-button link="" :to="coachDetailsLink">View Details</base-button>
         </div>
     </li>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router.js';
 import store from './store/index.js';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
