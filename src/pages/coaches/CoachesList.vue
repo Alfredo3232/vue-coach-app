d4e5cc58842c023fc1b89c791835d2027c4b2524
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-25 13:15:02 -0600

    add style to CoachesList

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -33,3 +33,16 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.controls {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
