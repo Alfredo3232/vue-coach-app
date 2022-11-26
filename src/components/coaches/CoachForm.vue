c826680c03c1038178be0746165bc9c54e112f46
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-26 16:09:04 -0600

    add coach registration feature

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,69 @@
+<template>
+    <form @submit.prevent="submitForm">
+        <div class="form-control">
+            <label for="firstname">First Name:</label>
+            <input type="text" id="firstname" v-model.trim="firstName" />
+        </div>
+
+        <div class="form-control">
+            <label for="lastname">Last Name:</label>
+            <input type="text" id="lastname" v-model.trim="lastName" />
+        </div>
+
+        <div class="form-control">
+            <label for="description">Description:</label>
+            <textarea id="description" rows="5" v-model.trim="description"></textarea>
+        </div>
+
+        <div class="form-control">
+            <label for="rate">Hourly Rate:</label>
+            <input type="number" id="rate" v-model.number="rate" />
+        </div>
+
+        <div class="form-control">
+            <h3>Areas of Expertise</h3>
+            <div>
+                <label for="frontend">Frontend Development</label>
+                <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+            </div>
+
+            <div>
+                <label for="backend">Backend Development</label>
+                <input type="checkbox" id="backend" value="backend" v-model="areas" />
+            </div>
+
+            <div>
+                <label for="career">Career Advisory</label>
+                <input type="checkbox" id="career" value="career" v-model="areas" />
+            </div>
+        </div>
+        <base-button>Register</base-button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            firstName: '',
+            lastName: '',
+            description: '',
+            rate: null,
+            areas: []
+        };
+    },
+    methods: {
+        submitForm() {
+            const formData = {
+                first: this.firstName,
+                last: this.lastName,
+                desc: this.description,
+                rate: this.rate,
+                areas: this.areas
+            };
+
+            console.log(formData);
+        }
+    }
+}
+</script>
--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,16 @@
 <template>
-    CoachRegister Page
+    <section>
+        <h2>Register as a coach now!</h2>
+        <coach-form></coach-form>
+    </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+    components: {
+        'coach-form': CoachForm
+    }
+};
+</script>
