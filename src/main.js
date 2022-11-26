4130bf5bc015cbf1539687bedb7c3b464682412a
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-25 19:42:36 -0600

    implement filter feature and tons more

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router/router.js';
 import store from './store/index.js';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
 
 const app = createApp(App);
 
@@ -13,5 +14,6 @@ app.use(store);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
