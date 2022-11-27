3bbee305a2df408686d8532af44bde21d1c57df8
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-26 21:12:25 -0600

    add contact functionality

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,42 @@
 <template>
-    RequestsReceived Page
+    <section>
+        <base-card>
+            <header>
+                <h2>Requests Received</h2>
+            </header>
+
+            <ul></ul>
+            <h3>You haven't received any requests yet!</h3>
+        </base-card>
+    </section>
 </template>
+
+<script>
+export default {
+    computed: {
+        receivedRequests() {
+            return this.$store.getters['requests/requests'];
+        },
+        hasRequests() {
+            return this.$store.getters['requests/hasRequests'];
+        }
+    }
+};
+</script>
+
+<style scoped>
+header {
+    text-align: center;
+}
+
+ul {
+    list-style: none;
+    margin: 2rem auto;
+    padding: 0;
+    max-width: 30rem;
+}
+
+h3 {
+    text-align: center;
+}
+</style>
