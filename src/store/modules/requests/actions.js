9af73c1a354265e47175b92a7df4334655d73b14
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-29 14:55:36 -0600

    contactCoach action post to database

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,12 +1,23 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
             coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
         };
 
-        fetch(`https://vue-coach-app-54e87-default-rtdb.firebaseio.com/requests/${}.json`);
+        const response = await fetch(`https://vue-coach-app-54e87-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(response.formData.message || 'Failed to send request.');
+            throw error;
+        }
+
+        newRequest.id = responseData.name;
 
         context.commit('addRequest', newRequest);
     }
