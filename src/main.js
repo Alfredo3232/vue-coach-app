55b2ace295e468cb84135c325ba7fa7b86f6a0f9
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-29 19:57:24 -0600

    fix bug typo

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-badge', BaseBadge);
 app.component('base-spinner', BaseSpinner);
-app.component('base-dialoh', BaseDialog);
+app.component('base-dialog', BaseDialog);
 
 
 app.mount('#app');
