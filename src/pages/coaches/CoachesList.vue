8a2e942a524116a410de79700916a18bbba9cecf
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-29 19:58:56 -0600

    add functionality client fetches db

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -10,7 +10,7 @@
     <section>
         <base-card>
             <div class="controls">
-                <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
+                <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
                 <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
             </div>
 
@@ -80,10 +80,10 @@ export default {
         setFilters(updatedFilters) {
             this.activeFilters = updatedFilters;
         },
-        async loadCoaches() {
+        async loadCoaches(refresh = false) {
             this.isLoading = true;
             try {
-                await this.$store.dispatch('coaches/loadCoaches');
+                await this.$store.dispatch('coaches/loadCoaches', { forceRefresh: refresh });
             } catch (error) {
                 this.error = error.message || 'Something went wrong!';
             }
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -25,7 +25,11 @@ export default {
             id: userId
         });
     },
-    async loadCoaches(context) {
+    async loadCoaches(context, payload) {
+        if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+            return;
+        }
+
         const response = await fetch(`https://vue-coach-app-54e87-default-rtdb.firebaseio.com/coaches.json`);
         const responseData = await response.json();
 
@@ -50,5 +54,6 @@ export default {
         }
 
         context.commit('setCoaches', coaches);
+        context.commit('setFetchTimestamp');
     }
 };
