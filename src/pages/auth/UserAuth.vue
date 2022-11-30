ec7d07b9f4ecfde0c3b3ee6d8beb71abe8f02b13
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-30 17:49:22 -0600

    add client side auth logic

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,22 +1,71 @@
 <template>
-    <base-card>
+    <base-card @submit.prevent="submitForm">
         <form>
 
             <div class="form-control">
                 <label for="email">E-Mail</label>
-                <input type="email" id="email" />
+                <input type="email" id="email" v-model.trim="email" />
             </div>
 
             <div class="form-control">
                 <label for="password">Password</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" v-model.trim="password" />
             </div>
 
-            <base-button>Login</base-button>
-            <base-button type="button" mode="flat">Signup instead</base-button>
+            <p v-if="!formIsValid">
+                Please enter a valid email and password (must be at least 6 characters long).
+            </p>
+
+            <base-button>{{ submitButtonCaption }}</base-button>
+            <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
         </form>
     </base-card>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            formIsValid: true,
+            mode: 'login'
+        };
+    },
+    computed: {
+        submitButtonCaption() {
+            if (this.mode === 'login') {
+                return 'Login';
+            } else {
+                return 'Signup';
+            }
+        },
+        switchModeButtonCaption() {
+            if (this.mode === 'login') {
+                return 'Signup instead';
+            } else {
+                return 'Login instead';
+            }
+        }
+    },
+    methods: {
+        submitForm() {
+            this.formIsValid = true;
+            if (this.email === '' || !this.email.includes('@') || this.password.length < 6) {
+                this.formIsValid = false;
+                return;
+            }
+        },
+        switchAuthMode() {
+            if (this.mode === 'login') {
+                this.mode = 'signup';
+            } else {
+                this.mode = 'login';
+            }
+        }
+    }
+};
+</script>
 
 <style scoped>
 form {
