43c1b82bb111f72581a93854fb10f151dc18f7fe
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-30 18:02:02 -0600

    add auth store module

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,18 @@ import { createStore } from "vuex";
 
 import coachesModule from "./modules/coaches/index.js";
 import requestsModule from "./modules/requests/index.js";
+import authModule from './modules/auth/index.js';
 
 const store = createStore({
     modules: {
         coaches: coachesModule,
-        requests: requestsModule
+        requests: requestsModule,
+        auth: authModule
     },
     state() {
         return {
             userId: 'c3'
         };
-    },
-    getters: {
-        userId(state) {
-            return state.userId;
-        }
     }
 });
 
