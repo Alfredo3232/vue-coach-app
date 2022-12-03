4e7cea3d1d557c3bcbbab8d2b14de4f5c4af4d12
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-02 19:57:56 -0600

    add routing auth requirements

--- src/router/router.js
+++ src/router/router.js
@@ -7,6 +7,7 @@ import ContactCoach from '../pages/requests/ContactCoach.vue';
 import RequestsReceived from '../pages/requests/RequestsReceived.vue';
 import NotFound from '../pages/NotFound.vue';
 import UserAuth from '../pages/auth/UserAuth.vue';
+import store from './store/index.js';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,11 +22,21 @@ const router = createRouter({
                 { path: 'contact', component: ContactCoach } // /coaches/:id/contact
             ]
         },
-        { path: '/register', component: CoachRegistration },
-        { path: '/requests', component: RequestsReceived },
-        { path: '/auth', component: UserAuth },
+        { path: '/register', component: CoachRegistration, meta: { requiresAuth: true } },
+        { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+        { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
         { path: '/:notFound(.*)', component: NotFound }
     ]
+});
+
+router.beforeEach((to, _, next) => {
+    if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next('/auth');
+    } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+        next('/coaches');
+    } else {
+        next();
+    }
 });
 
 export default router;
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,27 +1,5 @@
 export default {
     async login(context, payload) {
-        const response = await fetch('https://securetoken.googleapis.com/v1/token?key==AIzaSyDe-pvTQZV7QVU7mJa_lkhlcNhUXocdC-g', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        });
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            console.log(responseData);
-            const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
-            throw error;
-        }
-
-        console.log(responseData);
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
-        });
     },
     async signup(context, payload) {
         const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyDe-pvTQZV7QVU7mJa_lkhlcNhUXocdC-g', {
@@ -47,6 +25,31 @@ export default {
             tokenExpiration: responseData.expiresIn
         });
     },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        const response = await fetch('https://securetoken.googleapis.com/v1/token?key==AIzaSyDe-pvTQZV7QVU7mJa_lkhlcNhUXocdC-g', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            console.log(responseData);
+            const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
+            throw error;
+        }
+
+        console.log(responseData);
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        });
+    }
     logout(context) {
         context.commit('setUser', {
             token: null,
