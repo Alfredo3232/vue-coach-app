7f228cb5c3dab9e5cc503694f4c82af77be45af9
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-03 13:18:36 -0600

    add expiration to token

--- src/store/modules/auth/mutations.js
+++ src/store/modules/auth/mutations.js
@@ -2,6 +2,5 @@ export default {
     setUser(state, payload) {
         state.token = payload.token;
         state.userId = payload.userId;
-        state.tokenExpiration = payload.tokenExpiration;
     }
 };
